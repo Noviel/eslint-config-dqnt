59591dd08bc64cfcd3d92d738d19e065c0ebe0b4
Aleksandr Snov <snov.contact@gmail.com>
2017-10-20 02:12:26 +0700

    Add prettier, remove old stuff

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "globals": {
-    "Promise": true,
-    "Map": true,
-    "specs": true
-  },
-  "env": {
-    "browser": true,
-    "node": true
-  },
-  "plugins": [
-    "react"
-  ],
   "extends": "react-app",
+  "plugins": [
+    "prettier"
+  ],
   "rules": {
-    "arrow-parens": [
-      2,
-      "as-needed"
+    "no-extra-semi": "error",
+    "no-console": "warn",
+    "no-var": "error",
+    "one-var": ["error", "never"],
+    "no-unused-vars": "error",
+    "no-return-assign": "warn",
+    "object-shorthand": [
+      "error", 
+      "always",
+      { "avoidExplicitReturnArrows": true }
     ],
-    "no-console": 0,
-    "keyword-spacing": 2,
-    "newline-per-chained-call": [2, {"ignoreChainWithDepth": 2 }],
-    "no-extra-semi": 2,
-    "no-mixed-operators": 0,
-    "no-return-assign": 2,
-    "no-unused-vars": 2,
-    "no-var": 2,
-    "object-shorthand": [2, "methods"],
-    "one-var": [2, "never"],
-    "prefer-const": ["error", {
-      "destructuring": "all",
-      "ignoreReadBeforeAssign": false
-    }],
-    "semi": [
-      2,
-      "always"
+    "prefer-const": [
+      "error", 
+      { "destructuring": "all" }
     ],
-    "react/jsx-uses-vars": 2,
-    "react/jsx-uses-react": 2
+    "semi": [ "error", "always" ],
+    "prettier/prettier": "error"
   }
-};
+}
